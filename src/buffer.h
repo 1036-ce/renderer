0f203cc96ca601e3abb330279c96afb2121151a4
1036-ce <2376030467@qq.com>
2023-02-08 13:14:18 +0800

    add buffer.h

---
+++ src/buffer.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <vector>
+#include <cassert>
+#include "tgaimage.h"
+
+template<typename T> class Buffer {
+public:
+	Buffer() = default;
+	Buffer(int width, int height, int simples = 1)
+		: w(width), h(height), simples(simples), 
+		data(width * height, std::vector<T>(simples, T())) {}
+	T 	 get(int x, int y, int nthsimple);
+	void set(int x, int y, int nthsimple, T t);
+	// return average value in (x, y)
+	T    get_value(int x, int y);
+	int  width()  { return w; }
+	int  height() { return h; }
+private:
+	int w;
+	int h;
+	int simples;
+	std::vector<std::vector<T>> data;
+};
+
+template <typename T> 
+inline T Buffer<T>::get(int x, int y, int nthsimple) {
+	assert(nthsimple >= 0 && nthsimple < simples);
+	return data[y * w + x][nthsimple];
+}
+
+template <typename T>
+inline void Buffer<T>::set(int x, int y, int nthsimple, T t) {
+	assert(nthsimple >= 0 && nthsimple < simples);
+	data[y * w + x][nthsimple] = t;
+}
+
+template <typename T> inline T Buffer<T>::get_value(int x, int y) {
+	std::vector<T>& v = data[y * w + x];
+	T ret;
+	for (T &t: v)
+		ret = ret + t * (1.0 / simples);
+	return ret;
+}
